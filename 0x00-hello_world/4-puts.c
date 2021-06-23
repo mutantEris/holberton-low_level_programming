a7ea7ebc788f5e6ec37d17b0a38df95b3662256b
mutantEris <mmires@live.com>
2021-06-23 11:47:13 -0700

    ill puts a description later

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - puts some words and a newline
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
