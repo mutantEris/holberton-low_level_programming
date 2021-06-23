048d2cb687966035cb4c1fb3a8be4ac0c41f1719
mutantEris <mmires@live.com>
2021-06-23 13:16:50 -0700

    changed 2d to lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-  puts("Size of a char: %2d byte(s)", sizeof(char));
-  puts("Size of an int: %2d byte(s)"), sizeof(int));
-puts("Size of a long int: %2d byte(s)"), sizeof(long int));
-puts("Size of a long long int: %2d byte(s)"), sizeof(long long int));
-puts("Size of a float: %2d byte(s)");, sizeof(float));
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu byte(s)", sizeof(int));
+printf("Size of a long int: %lu byte(s)", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+printf("Size of a float: %lu byte(s)", sizeof(float));
 return (0);
 }
