f17bb6d377ca5afe1ec95367fdc2e2a9e228197b
mutantEris <mmires@live.com>
2021-06-23 13:08:23 -0700

    added sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <conio.h>
 /**
  * main - prints sizes
  * Return: 0
  */
 int main(void)
 {
-puts("Size of a char: %2d byte(s)");
-puts("Size of an int: %2d byte(s)");
-puts("Size of a long int: %2d byte(s)");
-puts("Size of a long long int: %2d byte(s)");
-puts("Size of a float: %2d byte(s)");
+  puts("Size of a char: %2d byte(s)", sizeof(char));
+  puts("Size of an int: %2d byte(s)"), sizeof(int));
+puts("Size of a long int: %2d byte(s)"), sizeof(long int));
+puts("Size of a long long int: %2d byte(s)"), sizeof(long long int));
+puts("Size of a float: %2d byte(s)");, sizeof(float));
 return (0);
 }
