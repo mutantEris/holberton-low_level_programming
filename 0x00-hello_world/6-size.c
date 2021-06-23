a8f3423fe7e7a27183ffdefc471039df45695d83
mutantEris <mmires@live.com>
2021-06-23 12:53:50 -0700

    tells you sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints sizes
+ * Return: 0
+ */
+int main(void)
+{
+puts("Size of a char: %2d byte(s)");
+puts("Size of an int: %2d byte(s)");
+puts("Size of a long int: %2d byte(s)");
+puts("Size of a long long int: %2d byte(s)");
+puts("Size of a float: %2d byte(s)");
+return (0);
+}
