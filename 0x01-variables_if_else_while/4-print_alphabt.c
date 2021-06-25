10daa8f6fd64c6f7d9bea5b14cb49af2c35ddef2
mutantEris <mmires@live.com>
2021-06-25 15:47:10 -0700

    prints SOME of the alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - writes the alphabet
+ * Return:0
+ */
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != ('e' || 'q'))
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
