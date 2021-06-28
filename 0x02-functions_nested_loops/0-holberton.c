f4724efa08df73d654db64874ff327bd0595f9cd
mutantEris <mmires@live.com>
2021-06-28 15:03:51 -0700

    fixed code

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
+#include "holberton.h"
 /**
- * main - prints Holberton
+ * main - prints holberton
  * Return:0
  */
 
 int main(void)
 {
-H = Holberton;
-putchar ("H");
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+return(0);
 }
