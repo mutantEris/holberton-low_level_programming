8a822c196227d9898a95cab716efa408c1f80f08
mutantEris <mmires@live.com>
2021-06-29 14:37:23 -0700

    prints base 16 characters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - writes the alphabet
+ * Return:0
+ */
+int main(void)
+{
+int g;
+for(g = 0; g < 10; g++)
+putchar(g + '0');
+for (g = 'a'; g <= 'f'; g++)
+putchar(g);
+putchar('\n');
+return (0);
+}
