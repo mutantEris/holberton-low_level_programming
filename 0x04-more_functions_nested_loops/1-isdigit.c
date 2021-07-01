111285124b586d968c45b15bd380cf363ae1d5dc
mutantEris <mmires@live.com>
2021-07-01 16:01:26 -0700

    checks if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - checks if digit
+ * @c: variable
+ * Return: 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
