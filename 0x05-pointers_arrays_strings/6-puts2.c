010267e086829a0ce017c6020d7fa17541cf4008
mutantEris <mmires@live.com>
2021-07-05 00:18:15 -0700

    prints every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - prints string then newline
+ * @*str: string
+ *  Return: 0
+ */
+void puts2(char *str)
+{
+while(*str)
+{
+_putchar(*str);
+str++;
+str++;
+}
+_putchar('\n');
+}
