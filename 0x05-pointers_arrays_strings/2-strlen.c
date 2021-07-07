26232120a65bb144862afc799183e887f7f7f0a8
mutantEris <mmires@live.com>
2021-07-06 17:26:21 -0700

    kernal and basic fixes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,15 @@
 
 /**
  * _strlen - checks length of string
- * @*s
+ * @s: variable
  *  Return: 0
  */
 int _strlen(char *s)
 {
 int length = 0;
-char *str;
-while(*str++)
+while (*s++)
 {
 length++;
 }
-return length
+return (length);
 }
