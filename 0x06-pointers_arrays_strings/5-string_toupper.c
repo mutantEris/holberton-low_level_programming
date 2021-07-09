dbc6b4f41ce9df7bf7da53940143788918481b3f
mutantEris <mmires@live.com>
2021-07-09 14:02:20 -0700

    changes lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * *string_toupper - reverses array
+ * @x: pointer to string
+ * Return: x
+ */
+char *string_toupper(char *x)
+{
+int g;
+for (g = 0; x[g] != '\0'; x++)
+if (x[g] >= 97 && x[g] <= 122)
+x[g] -= 32;
+return (x);
+}
