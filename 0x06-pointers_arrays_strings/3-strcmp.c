fd7bd6619949673c48b5c39434d2afd6e2b6b69e
mutantEris <mmires@live.com>
2021-07-09 13:11:42 -0700

    compares strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * *_strcmp - compares two string
+ * @s1: first string
+ * @s2: second string
+ *  Return: difference
+ */
+int *_strcmp(char *s1, char *s2)
+{
+int x;
+for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
+{
+if (s1[x] != s2[x])
+break;
+}
+return (s1[x] - s2[x]);
+}
