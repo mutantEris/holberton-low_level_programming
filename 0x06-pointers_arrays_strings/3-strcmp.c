4b9deea1e8c955b6a149ee51678b28d3ff43fbdf
mutantEris <mmires@live.com>
2021-07-09 13:16:30 -0700

    compares two strings but not broken this time

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 /**
- * *_strcmp - compares two string
+ * _strcmp - compares two string
  * @s1: first string
  * @s2: second string
  *  Return: difference
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int x;
 for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
