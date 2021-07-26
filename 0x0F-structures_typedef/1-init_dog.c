e72c3e0f4119088e39aaa513471eb9634199d8c1
mutantEris <mmires@live.com>
2021-07-26 12:06:40 -0700

    dogs are good

---
+++ 0x0F-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - makes a dog
+ * @d: dog
+ * @name: dogs name
+ * @age: how old doggy is
+ * @owner: dogs best friend
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
