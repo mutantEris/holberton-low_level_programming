762645d92206a5b37bde7d6495ca5853b6113366
mutantEris <mmires@live.com>
2021-07-26 11:29:05 -0700

    doggy

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -1,15 +1,15 @@
-#ifndef
+#ifndef DOG_H
 #define DOG_H
 /**
- * struct dog 
- * age = dogs age
- * owner = dogs friend
- * name = dogs name
+ * struct dog - good boy
+ * @age: dogs age
+ * @owner: dogs friend
+ * @name: dogs name
  */
 struct dog
 {
 float age;
-char * owner;
-char * name;
+char *owner;
+char *name;
 };
 #endif
