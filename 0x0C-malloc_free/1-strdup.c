ea6c039992e1d9b0f77a7990e00f108a85cc5b16
mutantEris <mmires@live.com>
2021-07-28 08:14:38 -0700

    returns pointer

--- 0x0C-malloc_free/1-strdup.c
+++ 0x0C-malloc_free/1-strdup.c
@@ -12,17 +12,17 @@ int g;
 char *anything;
 int x;
 
-if(str == NULL)
-return(NULL);
+if (str == NULL)
+return (NULL);
 for (g = 0; str[g] != '\0'; g++)
 {
 }
 anything = malloc((g + 1) * sizeof(char));
 if (anything == NULL)
-return(NULL);
+return (NULL);
 for (x = 0; x <= g; x++)
 {
 anything[x] = str[x];
 }
-return(anything);
+return (anything);
 }
