4e9d4b6cf491f72a05a8951aa875e874b375cdfc
mutantEris <mmires@live.com>
2021-07-28 08:13:29 -0700

    returns pointer

---
+++ 0x0C-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to new memory space copy
+ * @str: string
+ * Return: NULL or anything
+ */
+char *_strdup(char *str)
+{
+int g;
+char *anything;
+int x;
+
+if(str == NULL)
+return(NULL);
+for (g = 0; str[g] != '\0'; g++)
+{
+}
+anything = malloc((g + 1) * sizeof(char));
+if (anything == NULL)
+return(NULL);
+for (x = 0; x <= g; x++)
+{
+anything[x] = str[x];
+}
+return(anything);
+}
