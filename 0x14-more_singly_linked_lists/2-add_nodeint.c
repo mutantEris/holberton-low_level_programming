7b84b7218dcf2b8994912d9de3d068a5bc6d7a04
mutantEris <mmires@live.com>
2021-08-11 14:24:06 -0700

    betty defiant

--- 0x14-more_singly_linked_lists/2-add_nodeint.c
+++ 0x14-more_singly_linked_lists/2-add_nodeint.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * add_nodeint - adds nodes
+ * @head: the head honcho node
+ * @n: number
+ * Return: NULL or g
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *g;
