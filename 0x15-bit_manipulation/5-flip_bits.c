bc208bdf2e88ec51b91fd46dd6802f0d34a2c919
mutantEris <mmires@live.com>
2021-08-12 21:15:29 -0700

    betty defiant

--- 0x15-bit_manipulation/5-flip_bits.c
+++ 0x15-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
+/**
+ * flip_bits - flips them bits
+ * @n: int 1
+ * @m: int 2
+ * Return: count
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 unsigned long int g;
 int count = 0;
 
-g = n ^ m;  
+g = n ^ m; 
 while (g)
 {
 count += (g & 1);
