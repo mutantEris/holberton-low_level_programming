584c5221949e8d96bf41b1e730f6b37be5e27419
mutantEris <mmires@live.com>
2021-08-12 22:04:34 -0700

    compile

--- 0x15-bit_manipulation/2-get_bit.c
+++ 0x15-bit_manipulation/2-get_bit.c
@@ -5,7 +5,8 @@
  * @index: index
  * Return: -1 or 1
  */
-int get_bit(unsigned long int n, unsigned int index);
+
+int get_bit(unsigned long int n, unsigned int index)
 {
 if (index > 63)
 return (-1);
