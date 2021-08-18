de0a7f4126b46f3b589d969bf08bb537acfb1fb3
mutantEris <mmires@live.com>
2021-08-18 16:00:48 -0700

    creates files

---
+++ 0x16-file_io/1-create_file.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+int create_file(const char *filename, char *text_content)
+{
+int w, o, buffy = 0;
+
+if (filename == NULL)
+return (-1);
+if (text_content == NULL)
+text_content = "";
+while (text_content[buffy])
+buffy++;
+o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+if (o == -1)
+return (-1);
+w = write(o, text_content, buffy);
+if (w == -1)
+return (-1);
+close(o);
+return (1);
+}
