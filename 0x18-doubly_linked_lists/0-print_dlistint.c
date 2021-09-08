03dc844fae250eacd1f8f7522f4a0764c4ab6907
mutantEris <mmires@live.com>
2021-09-08 13:24:41 -0700

    lists elments

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_dlistint - prints all elements of list
+ * @h: h
+ * Return: g
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+int g = 0;
+list_t *tmp = (list_t *)h;
+while (tmp != NULL)
+{
+if (tmp->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", tmp->len, tmp->str);
+tmp = tmp->next;
+g++;
+}
+return (g);
+}
