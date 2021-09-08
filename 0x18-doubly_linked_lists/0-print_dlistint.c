f92d4c71573a26155a44e0d03ff89287547ae347
mutantEris <mmires@live.com>
2021-09-08 15:07:39 -0700

    betty

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -8,15 +8,9 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 int g = 0;
-list_t *tmp = (list_t *)h;
-while (tmp != NULL)
-{
-if (tmp->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", tmp->len, tmp->str);
-tmp = tmp->next;
-g++;
-}
+
+const dlistint_t *tmp = h;
+for (; tmp != NULL; tmp = tmp->next, g++)
+printf("%d\n", tmp->n);
 return (g);
 }
