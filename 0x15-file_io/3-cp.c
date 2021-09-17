86ffd95cd1c2b50c3dfeba29fac6c534ac970983
mutantEris <mmires@live.com>
2021-09-16 18:51:27 -0700

    broken

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+/**
+ * main - copies file
+ * @argc: argument count
+ * @argv: argument variables
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int from, to, count;
+char buffer[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97); }
+from = open(argv[1], O_RDONLY);
+if (from == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't readfrom file %s\n", argv[1]);
+exit(98); }
+to = open(argv[2], O_WRONLY | O_CREAT);
+if (to == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+exit(99); }
+while ((count = read(from, buffer, 1024)) > 0)
+{
+if (write(to, buffer, count) != count)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+exit(99); } }
+if (count == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't readfrom file %s\n", argv[1]);
+exit(98); }
+if (close(from) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d", from);
+exit(100); }
+if (close(to) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %d", to);
+exit(100); }
+return (0); }
