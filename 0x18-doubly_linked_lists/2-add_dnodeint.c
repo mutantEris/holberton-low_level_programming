45469c9541ecbee4bac0f688075ba127991044c4
mutantEris <mmires@live.com>
2021-09-20 16:35:04 -0700

    adds nodes

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_dnodeint - adds nodes
+ * @head: front node
+ * @n: number
+ * Return: NULL or g
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *nnode;
+
+nnode = malloc(sizeof(dlistint_t));
+if (nnode == NULL)
+return (NULL);
+if (*head == NULL)
+nnode->next = NULL;
+else
+nnode->next = *head;
+nnode->n = n;
+*head = nnode;
+return (*head);
+}
