572f4e6059e762b530a81d03c221f7420df820f5
mutantEris <mmires@live.com>
2022-02-17 12:23:30 -0600

    The money was stolen

--- 0x00-hash_tables/1-djb2.c
+++ 0x00-hash_tables/1-djb2.c
@@ -1,13 +1,13 @@
 #include "hash_tables.h"
 
-unsigned long int hash_djb2(const unsigned char *str)
-{
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
  *
  * Return: hash value
  */
+
+unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
 	int c;
