df748f7c90ce6ea8b1ba9469265e2fa9c160385f
mutantEris <mmires@live.com>
2022-03-17 13:01:01 -0500

    Algorithmic

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - performs a linear search
+ * @array: the array to search
+ * @size: array size
+ * @value: sought value
+ * Return: where value is found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int x;
+	if (!array || size == 0)
+		return (-1);
+	for (x = 0; x < (int)size; x++)
+	{
+		printf("Value checked array[%i] = [%i]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+}
